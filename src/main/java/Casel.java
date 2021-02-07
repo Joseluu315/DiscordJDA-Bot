bd2d75af00f94d8041d691ed41891d0fcc936e59
Joseluu315 <77939750+Joseluu315@users.noreply.github.com>
2021-02-07 13:27:56 +0100

    Add files via upload

---
+++ src/main/java/Casel.java
@@ -0,0 +1,14 @@
+import manager.StartCasel;
+import javax.security.auth.login.LoginException;
+
+
+/**
+ * Project: Casel
+ * Date: 06/02/2021 @ 22:53
+ * Class: StartBot
+ */
+public class Casel {
+    public static void main(String[] args) throws LoginException {
+        StartCasel.INTANCE.startBot();
+    }
+}
---
+++ src/main/java/manager/StartCasel.java
@@ -0,0 +1,32 @@
+package manager;
+
+import com.github.m5rian.jdaCommandHandler.CommandListener;
+import com.github.m5rian.jdaCommandHandler.commandServices.DefaultCommandService;
+import com.github.m5rian.jdaCommandHandler.commandServices.DefaultCommandServiceBuilder;
+import commands.PingCommand;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * Project: Casel
+ * Date: 06/02/2021 @ 21:59
+ * Class: manager.Main
+ */
+public enum StartCasel {
+    INTANCE;
+    private JDA jda;
+    public void startBot() throws LoginException {
+        DefaultCommandService commandService = new DefaultCommandServiceBuilder()
+                .setDefaultPrefix("/")
+                .build();
+
+        JDA jda = JDABuilder.createDefault("ODA2MjY3MDIzMzE1MTczMzc3.YBm8rg.DTilyylxIWIqVXm8oMSp94Cl9B8")
+                .setActivity(Activity.watching("In 2 Server" + " | /help"))
+                .addEventListeners(new CommandListener(commandService))
+                .build();
+        commandService.registerCommand(new PingCommand());
+    }
+}
